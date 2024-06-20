0f27f6b94044d22c831f64019c0f12e8db9f4362
Ege Güvenir <egeguvenirdev@gmail.com>
2024-06-21 00:40:38 +0300

    Bench & Hoops

--- Assets/Scripts/BasketballHoop.cs
+++ Assets/Scripts/BasketballHoop.cs
@@ -7,12 +7,16 @@ using TMPro;
 
 public class BasketballHoop : ShootableObjectBase
 {
+    [SerializeField] private SkinnedMeshRenderer mesh;
+    private Tweener smoothTween;
     private Tween tween;
+    private float smoothMoneyNumbers = 0f;
 
     protected override void GetBallValue(float ballValue)
     {
         tween.Kill();
-        tween = transform.DOPunchScale(Vector3.one * 0.05f, 0.5f, 6).SetUpdate(true);
+        tween = transform.DOPunchScale(Vector3.one * 0.025f, 0.5f, 6).SetUpdate(true);
+        NetFlopEffect();
 
         if (value >= 0) value -= ballValue;
         if (value < 0) value += 1;
@@ -24,5 +28,28 @@ public class BasketballHoop : ShootableObjectBase
         {
             Destroy(gameObject);
         }
+    }
+
+    private void NetFlopEffect()
+    {
+        smoothTween.Kill();
+        smoothMoneyNumbers = 0;
+        mesh.SetBlendShapeWeight(0, 0f);
+
+        smoothTween = DOTween.To(() => smoothMoneyNumbers, x => smoothMoneyNumbers = x, 100f, 0.2f).SetSpeedBased(false)
+            .OnUpdate(() => 
+            { 
+                mesh.SetBlendShapeWeight(0, smoothMoneyNumbers); 
+            })
+
+            .OnComplete(() => 
+            {
+                smoothTween = DOTween.To(() => smoothMoneyNumbers, x => smoothMoneyNumbers = x, 0, 0.2f).SetSpeedBased(false)
+                    .OnUpdate(() =>
+                    {
+                        mesh.SetBlendShapeWeight(0, smoothMoneyNumbers);
+                    });
+            });
+        
     }
 }
